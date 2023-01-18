878ae3a3fe001e67c12cbd1d4360de62e8b218f4
Gurjant Singh <gurjant.singh@cirvianum.cat>
2023-01-18 14:56:02 +0100

    made editable

--- js/potential.js
+++ js/potential.js
@@ -26,20 +26,31 @@ function addContact() {
     // Create a new table row
     var newRow = document.createElement("tr");
 
+
     // Create the cells for the row
     var dniCell = document.createElement("td");
     var nameCell = document.createElement("td");
     var emailCell = document.createElement("td");
     var removeCell = document.createElement("td");
+    var editCell = document.createElement("td");
 
-
-    // Create the remove button
+    // Create the select button
     var selectbox = document.createElement("input");
     selectbox.type = "checkbox";
     selectbox.className = "selectBox";
 
-    // add the remove button to the remove cell
+    // add the select button to the remove cell
     removeCell.appendChild(selectbox);
+
+    // Create the edit button
+    var editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.className = "editButton";
+    // add the onclick event to the edit button
+    editButton.onclick = function () { editContact(this) };
+
+    // add the edit button to the edit cell
+    editCell.appendChild(editButton);
 
     // make the text content of the cells
     dniCell.textContent = dni;
@@ -51,6 +62,7 @@ function addContact() {
     newRow.appendChild(nameCell);
     newRow.appendChild(emailCell);
     newRow.appendChild(removeCell);
+    newRow.appendChild(editCell);
 
     // add this new row to the table
     var table = document.getElementById("contact-table");
@@ -62,6 +74,8 @@ function addContact() {
     potential.push({ name: name, email: email, dni: dni });
     localStorage.setItem("potential", JSON.stringify(potential));
 }
+
+
 
 window.onload = function () {
     var potential = JSON.parse(localStorage.getItem("potential")) || [];
@@ -77,6 +91,7 @@ window.onload = function () {
         var nameCell = document.createElement("td");
         var emailCell = document.createElement("td");
         var removeCell = document.createElement("td");
+        var editCell = document.createElement("td");
 
         // Create the remove checkbox
         var selectbox = document.createElement("input");
@@ -85,6 +100,15 @@ window.onload = function () {
 
         // add the remove checkbox to the remove cell
         removeCell.appendChild(selectbox);
+        // Create the edit button
+        var editButton = document.createElement("button");
+        editButton.textContent = "Edit";
+        editButton.className = "editButton";
+        // add the onclick event to the edit button
+        editButton.onclick = function () { editContact(this) };
+
+        // add the edit button to the edit cell
+        editCell.appendChild(editButton);
 
         // Make the text content of the cells
         dniCell.textContent = potential[i].dni;
@@ -96,6 +120,8 @@ window.onload = function () {
         newRow.appendChild(nameCell);
         newRow.appendChild(emailCell);
         newRow.appendChild(removeCell);
+        newRow.appendChild(editCell);
+
 
         // Add the new row to the table
         tbody.appendChild(newRow);
@@ -132,4 +158,58 @@ function mailSelected() {
         var emailLink = "mailto:" + selectedEmails.join(";") + "?subject=Email from my website&body=Hello,%0D%0A%0D%0A";
         window.location.href = emailLink;
     }
+}
+
+function editContact(editButton) {
+    // Get the parent row of the clicked button
+    var row = editButton.parentNode.parentNode;
+
+    // Get the values of the cells in the row
+    var dni = row.cells[0].innerHTML;
+    var name = row.cells[1].innerHTML;
+    var email = row.cells[2].innerHTML;
+
+    // Open a form with the current values pre-filled
+    document.getElementById("edit-dni").value = dni;
+    document.getElementById("edit-name").value = name;
+    document.getElementById("edit-email").value = email;
+
+    // Show the form
+    document.getElementById("edit-form").style.display = "block";
+}
+
+// add a submit event listener to the form
+document.getElementById("edit-form").addEventListener("submit", saveChanges);
+
+function saveChanges(event) {
+    // prevent the form from submitting
+    event.preventDefault();
+
+    // get the updated values from the form
+    var dni = document.getElementById("edit-dni").value;
+    var name = document.getElementById("edit-name").value;
+    var email = document.getElementById("edit-email").value;
+
+    // find the contact in the local storage by dni
+    var potential = JSON.parse(localStorage.getItem("potential")) || [];
+    var contactIndex = potential.findIndex(contact => contact.dni === dni);
+
+    // update the contact information
+    potential[contactIndex].name = name;
+    potential[contactIndex].email = email;
+    potential[contactIndex].dni = dni;
+
+    // update the local storage
+    localStorage.setItem("potential", JSON.stringify(potential));
+
+    // update the table
+    var row = document.getElementById("edit-form").parentNode.parentNode;
+    row.cells[0].innerHTML = dni;
+    row.cells[1].innerHTML = name;
+    row.cells[2].innerHTML = email;
+
+    // close the form
+    document.getElementById("edit-form").style.display = "none";
+    
+    
 }
